25ff53ad17ef188d716f7c27249fdb22a7c4f48c
SoyLizarDev <lizardev@hotmail.com>
2026-02-23 13:45:11 -0400

    Actualización de sonido de notificación y del icono

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 const http = require('http');
 const { exec } = require('child_process');
+const path = require('path');
 
 const server = http.createServer((req, res) => {
     let body = '';
     req.on('data', chunk => { body += chunk.toString(); });
     req.on('end', () => {
+        const data = JSON.parse(body);
+        const rutaIcono = path.join(__dirname, './icon.png');
         console.log("Datos recibidos:", body);
         
         try {
             const data = JSON.parse(body);
-            const sonido = "./whatsapp.mp3"; // Asegúrate de que el archivo esté aquí
-            
-            // Unificamos: Sonido + Variable de Entorno + Notificación
-            // Usamos comillas simples en data.titulo/mensaje por seguridad
-            const comando = `paplay ${sonido} & DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/${process.getuid()}/bus notify-send '${data.titulo}' '${data.mensaje}' --icon=notification-message-im`;
+            const sonido = "./noti.mp3"; 
+            const comando = `paplay ${sonido} & DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/${process.getuid()}/bus notify-send '${data.titulo}' '${data.mensaje}' --icon=${rutaIcono}`;
             
             exec(comando, (error, stderr) => {
                 if (error) console.error(`Error de ejecución: ${error}`);
